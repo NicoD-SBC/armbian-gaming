11a32854c609c28047b78be9bb0fc87ab2302b5a
NicoD-SBC <46458369+NicoD-SBC@users.noreply.github.com>
2021-07-05 15:28:03 +0200

    Update armbian-gaming.sh

--- armbian-gaming.sh
+++ armbian-gaming.sh
@@ -4,7 +4,8 @@ function menu {
 echo "Hello. Please choose what you want to install ! "
 echo "1. Install all "
 echo "2. Install Box86 "
-echo "3. Exit armbian-gaming "
+echo "3. Install Box64 "
+echo "4. Exit armbian-gaming "
 
 read choicevar
 
@@ -14,8 +15,10 @@ if [ $choicevar -eq 1 ]
 elif [ $choicevar -eq 2 ]
 	then 
 	box86
-
-elif [ $choicevar -eq 3 ]
+elif [ $choicevar -eq 2 ]
+	then 
+	box64
+elif [ $choicevar -eq 4 ]
 	then
 	echo "Greetings, NicoD "
 	exit
@@ -39,12 +42,13 @@ function box86 {
 	sudo cmake .. -DRK3399=1 -DCMAKE_BUILD_TYPE=RelWithDebInfo
 	sudo make -j4
 	sudo make install
+	cd ~/
 }
 
 function dependencies {	
 #Install dependencies
-sudo apt -y install git cmake libncurses6:armhf libc6:armhf  libx11-6:armhf libgdk-pixbuf2.0-0:armhf libgtk2.0-0:armhf libstdc++6:armhf libsdl2-2.0-0:armhf mesa-va-drivers:armhf libsdl1.2-dev:armhf libsdl-mixer1.2:armhf libpng16-16:armhf libcal3d12v5:armhf libsdl2-net-2.0-0:armhf libopenal1:armhf libsdl2-image-2.0-0:armhf libvorbis-dev:armhf libcurl4:armhf libjpeg62:armhf  libudev1:armhf libgl1-mesa-dev:armhf  libx11-dev:armhf libsmpeg0:armhf libavcodec58:armhf libavformat58:armhf libswscale5:armhf libsdl2-image-2.0-0:armhf libsdl2-mixer-2.0-0:armhf gcc-arm-linux-gnueabihf cmake git cabextract
-debianDependenciesFix
+	sudo apt -y install git cmake libncurses6:armhf libc6:armhf  libx11-6:armhf libgdk-pixbuf2.0-0:armhf libgtk2.0-0:armhf libstdc++6:armhf libsdl2-2.0-0:armhf mesa-va-drivers:armhf libsdl1.2-dev:armhf libsdl-mixer1.2:armhf libpng16-16:armhf libcal3d12v5:armhf libsdl2-net-2.0-0:armhf libopenal1:armhf libsdl2-image-2.0-0:armhf libvorbis-dev:armhf libcurl4:armhf libjpeg62:armhf  libudev1:armhf libgl1-mesa-dev:armhf  libx11-dev:armhf libsmpeg0:armhf libavcodec58:armhf libavformat58:armhf libswscale5:armhf libsdl2-image-2.0-0:armhf libsdl2-mixer-2.0-0:armhf gcc-arm-linux-gnueabihf cmake git cabextract
+	debianDependenciesFix
 }
 
 function debianDependenciesFix {
@@ -52,8 +56,20 @@ function debianDependenciesFix {
 	 sudo apt -y --fix-broken install
 }
 
+function box64 {
+	git clone https://github.com/ptitSeb/box64
+	cd box64
+	mkdir build
+	cd build
+	cmake .. -DRK3399=1 -DCMAKE_BUILD_TYPE=RelWithDebInfo
+	make -j4
+	sudo make install
+	cd ~/
+}
+
 function all {
 	box86
+	box64
 }
 
 

2068ec2fea2a83c0785f83c9b724dfd8a5a2f2fe
monkaBlyat <54044929+monkaBlyat@users.noreply.github.com>
2024-05-01 16:33:11 +0200

    more dependency fixes

--- armbian-gaming.sh
+++ armbian-gaming.sh
@@ -155,7 +155,7 @@ function winetricksInstall {
 	cd ~
 	sudo dpkg --add-architecture armhf
 	sudo apt update
-	sudo apt install libxinerama-dev:armhf libxrandr-dev:armhf libxcomposite-dev:armhf libxi-dev:armhf libxcursor-dev:armhf mesa-va-drivers:armhf libc6:armhf libx11-6:armhf libgdk-pixbuf2.0-0:armhf libgl1-mesa-dev:armhf zenity libavcodec58:armhf libavformat58:armhf libpng16-16:armhf libcal3d12v5:armhf libopenal1:armhf libvorbis-dev:armhf libcurl4:armhf osspd:armhf libjpeg62:armhf libudev1:armhf libsnappy1v5:armhf libsmpeg0:armhf libmyguiengine3debian1v5:armhf libqt5core5a:armhf libavutil56:armhf libswresample3:armhf
+	sudo apt install libncurses6:armhf libc6:armhf libx11-6:armhf libgdk-pixbuf2.0-0:armhf libgtk2.0-0:armhf libstdc++6:armhf libsdl2-2.0-0:armhf mesa-va-drivers:armhf libsdl-mixer1.2:armhf libpng16-16:armhf libsdl2-net-2.0-0:armhf libopenal1:armhf libsdl2-image-2.0-0:armhf libjpeg62:armhf libudev1:armhf libgl1-mesa-dev:armhf libx11-dev:armhf libsdl2-image-2.0-0:armhf libsdl2-mixer-2.0-0:armhf libvulkan1:armhf
 	echo "assuming downloaded wine in /home/wine"
 	sudo ln -s ~/wine/bin/wine /usr/local/bin/wine
 	sudo ln -s ~/wine/bin/winecfg /usr/local/bin/winecfg
